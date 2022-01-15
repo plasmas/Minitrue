0e911e6760ababd787e53915d90d5f43bee67770
plasmas <plasticplasmas@outlook.com>
2022-01-15 15:56:55 -0500

    Initial Commit

---
+++ Minitrue/MinitrueApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinitrueApp.swift
+//  Minitrue
+//
+//  Created by Yuanqi Wang on 1/15/22.
+//
+
+import SwiftUI
+
+@main
+struct MinitrueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
